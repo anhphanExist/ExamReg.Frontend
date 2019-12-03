162e2d0888c52607e88761299250d15b2893f412
Dat Luu <tad.louis2810@gmail.com>
2019-12-03 09:20:19 +0700

    upgrade students view

--- coreui-free-vue-admin-template/src/views/main-admin/sidebar-views/Student.vue
+++ coreui-free-vue-admin-template/src/views/main-admin/sidebar-views/Student.vue
@@ -1,81 +1,25 @@
 <template>
-
-  <CCard>
-    <CCardHeader>
-      <slot name="header">
-        <CIcon name="cil-grid"/> {{caption}}
-      </slot>
-    </CCardHeader>
-    <CCardBody>
-      <CDataTable
-        :hover="hover"
-        :striped="striped"
-        :bordered="bordered"
-        :small="small"
-        :fixed="fixed"
-        :items="getShuffledUsersData()"
-        :fields="fields"
-        :items-per-page="small ? 10 : 5"
-        pagination
-      >
-      <template #action="{item}">
-          <td>
-            <CForm inline>
-                <CButton color="outline-success" class="mr-sm-4" type="submit">Reset Password</CButton>
-                <CButton color="outline-warning" class="mr-sm-4" type="submit">Edit</CButton>
-                <CButton color="outline-danger" class="my-2 my-sm-0" type="submit">Delete</CButton>
-            </CForm>
-          </td>
-      </template>
-      </CDataTable>
-    </CCardBody>
-  </CCard>
+  <div>
+    <CRow>
+      <CCol lg='12'>
+        <CTableWrapper
+          hover
+          striped
+          boredered
+          small
+          fixed
+        />
+      </CCol>
+    </CRow>
+  </div>
 </template>
 
 <script>
-import studentsData from './data/student'
+import CTableWrapper from './Tables/StudentTable.vue'
+
 export default {
-  name: 'Table',
-  props: {
-    fields: {
-      type: Array,
-      default () {
-        return ['mssv', 'fullname', 'username', 'email', 'action']
-      }
-    },
-    caption: {
-      type: String,
-      default: 'Student Table'
-    },
-    hover: Boolean,
-    striped: Boolean,
-    bordered: Boolean,
-    small: Boolean,
-    fixed: Boolean,
-    dark: Boolean
-  },
-  methods: {
-    shuffleArray (array) {
-      for (let i = array.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1))
-        let temp = array[i]
-        array[i] = array[j]
-        array[j] = temp
-      }
-      return array
-    },
-
-    getShuffledUsersData () {
-      return this.shuffleArray(studentsData.slice(0))
-    },
-
-    getBadge (status) {
-      return status === 'Active' ? 'success'
-        : status === 'Inactive' ? 'secondary'
-          : status === 'Pending' ? 'warning'
-            : status === 'Banned' ? 'danger' : 'primary'
-    }
-  }
-}
+  name: "Table",
+  components: {CTableWrapper},
+};
 </script>
 

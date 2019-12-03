c2315653eef299d01b97d10b373d75bb84566448
Dat Luu <tad.louis2810@gmail.com>
2019-12-03 21:20:32 +0700

    student add form

--- coreui-free-vue-admin-template/src/views/main-admin/content-views/Tables/StudentTable.vue
+++ coreui-free-vue-admin-template/src/views/main-admin/content-views/Tables/StudentTable.vue
@@ -10,11 +10,11 @@
             </div>
           </CCol>
           <CCol sm="7" class="d-none d-md-block">
-            <CDropdown 
-                class="float-right" 
-                toggler-text="More Action" 
-                color="outline-primary"
-                placement="bottom-end"
+            <CDropdown
+              class="float-right"
+              toggler-text="More Action"
+              color="outline-info"
+              placement="bottom-end"
             >
               <CDropdownItem>Import Student</CDropdownItem>
               <CDropdownItem>Download Student Template</CDropdownItem>
@@ -23,7 +23,7 @@
               <CDropdownItem>Download Student Term Template</CDropdownItem>
               <CDropdownItem>Export Student Term</CDropdownItem>
             </CDropdown>
-            <CButton color="outline-primary" @click="myModal = true" class="float-right mr-3">Add More</CButton>
+            <CButton color="outline-info" @click="myModal = true" class="float-right mr-3">Add More</CButton>
           </CCol>
         </CRow>
       </slot>
@@ -51,16 +51,73 @@
         </template>
       </CDataTable>
     </CCardBody>
-    <CModal
-      title="Add more students"
-      :show.sync="myModal"
-    >
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <CModal title="Add more students" :centered="true" :show.sync="myModal" color="info">
+      <CCol sm="12">
+        <CCard>
+          <CCardHeader>
+            <strong>Student Info</strong>
+          </CCardHeader>
+          <CCardBody>
+            <CRow>
+              <CCol sm="12">
+                <CInput label="MSSV" horizontal placeholder="00000000" />
+              </CCol>
+            </CRow>
+            <CRow>
+              <CCol sm="12">
+                <CInput horizontal label="First Name" placeholder="Enter student first name" />
+              </CCol>
+            </CRow>
+            <CRow>
+              <CCol sm="12">
+                <CInput horizontal label="Last Name" placeholder="Enter student last name" />
+              </CCol>
+            </CRow>
+            <CRow>
+              <CCol sm="12">
+                <CInput
+                  horizontal
+                  label="Password"
+                  type="password"
+                  placeholder="Enter student password"
+                />
+              </CCol>
+            </CRow>
+            <CRow>
+              <CCol sm="12">
+                <CInput
+                  label="Email input"
+                  placeholder="Enter student email"
+                  type="email"
+                  horizontal
+                  autocomplete="email"
+                />
+              </CCol>
+            </CRow>
+            <CRow>
+              <CCol sm="4">
+                <CSelect
+                  label="Day"
+                  :options="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]"
+                />
+              </CCol>
+              <CCol sm="4">
+                <CSelect label="Month" :options="[1,2,3,4,5,6,7,8,9,10,11,12]" />
+              </CCol>
+              <CCol sm="4">
+                <CSelect
+                  label="Year"
+                  :options="[1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009]"
+                />
+              </CCol>
+            </CRow>
+          </CCardBody>
+        </CCard>
+      </CCol>
+      <template #footer>
+        <CButton @click="myModal = false" color="outline-danger">Discard</CButton>
+        <CButton @click="myModal = false" color="outline-success">Accept</CButton>
+      </template>
     </CModal>
   </CCard>
 </template>
@@ -72,13 +129,13 @@ export default {
   data() {
     return {
       myModal: false
-    }
+    };
   },
   props: {
     fields: {
       type: Array,
       default() {
-        return ["mssv", "fullname", "DOB", "username", "email", "action"];
+        return ["mssv", "fullname", "DOB", "password", "email", "action"];
       }
     },
     caption: {

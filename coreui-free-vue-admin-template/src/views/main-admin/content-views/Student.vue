5f59158590451bc0712bac87980b76d589c8b6b0
Anh Phan <anhphan.csf@gmail.com>
2019-12-15 01:23:23 +0700

    add validate cho semester và sửa Cselect thành select thường (cần làm lại cho đẹp)

--- coreui-free-vue-admin-template/src/views/main-admin/content-views/Student.vue
+++ coreui-free-vue-admin-template/src/views/main-admin/content-views/Student.vue
@@ -140,25 +140,43 @@
             
             <CRow>
               <CCol sm="4">
-                <CSelect
-                        :options="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]"
-                        label="Day-DOB"
-                        v-model="student_add.dob.day"
-                />
+<!--                <CSelect-->
+<!--                        :options="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]"-->
+<!--                        label="Day-DOB"-->
+<!--                        v-model="student_add.dob.day"-->
+<!--                />-->
+                <label>
+                  Birth date
+                  <select v-model="student_add.dob.day">
+                    <option v-for="index in 31" :key="index">{{ index }}</option>
+                  </select>
+                </label>
               </CCol>
               <CCol sm="4">
-                <CSelect
-                        :options="[1,2,3,4,5,6,7,8,9,10,11,12]"
-                        label="Month-DOB"
-                        v-model="student_add.dob.month"
-                />
+<!--                <CSelect-->
+<!--                        :options="[1,2,3,4,5,6,7,8,9,10,11,12]"-->
+<!--                        label="Month-DOB"-->
+<!--                        v-model="student_add.dob.month"-->
+<!--                />-->
+                <label>
+                  Birth month
+                  <select v-model="student_add.dob.month">
+                    <option v-for="index in 12" :key="index">{{ index }}</option>
+                  </select>
+                </label>
               </CCol>
               <CCol sm="4">
-                <CSelect
-                        :options="[1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009]"
-                        label="Year-DOB"
-                        v-model="student_add.dob.year"
-                />
+<!--                <CSelect-->
+<!--                        :options="[1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009]"-->
+<!--                        label="Year-DOB"-->
+<!--                        v-model="student_add.dob.year"-->
+<!--                />-->
+                <label>
+                  Birth year
+                  <select v-model="student_add.dob.year">
+                    <option v-for="index in 50" :key="index">{{ index + 1970 }}</option>
+                  </select>
+                </label>
               </CCol>
             </CRow>
           </CCardBody>
